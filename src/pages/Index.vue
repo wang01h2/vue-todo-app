1bbce23f040c80e55ff5c548a79174f644162135
wang01h2 <1299839398@qq.com>
2022-06-22 07:38:14 +0800

    fix(Index.vue): margin适配不同的屏幕
     margin适配不同的屏幕

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="header-view w-auto">
-      <h1 class="text-5xl text-center mt-10"> 👋 Hello </h1>
-      <h2 class="text-4xl text-blue-500 text-center mt-6">ToDo</h2>
+      <h1 class="text-5xl text-center mt-10 xl:mt-10 2xl:mt-20"> 👋 Hello </h1>
+      <h2 class="text-4xl text-blue-500 text-center my-6 xl:my-6 2xl:my-12">ToDo</h2>
     </div>
   </div>
 </template>
