aacbd7d970edc295f615febfdb80c09a8e3df161
wang01h2 <1299839398@qq.com>
2022-06-21 21:00:57 +0800

    feat(Index.vue): 项目添加tailwindcss 与 Index.vue中使用
    项目添加tailwindcss 与 Index.vue中使用

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index";
+import "./css/tailwindcss.css"
 createApp(App).use(router).mount('#app')
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1> 👋 Hello </h1>
+  <div>
+    <div class="header-view w-auto">
+      <h1 class="text-5xl text-center mt-10"> 👋 Hello </h1>
+      <h2 class="text-4xl text-blue-500 text-center mt-6">Todos</h2>
+    </div>
+  </div>
+
 </template>
 
 <script setup lang="ts">
