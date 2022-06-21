3d5e1aabb7be4b1bb4f129457e87a831601a0381
wang01h2 <1299839398@qq.com>
2022-06-22 00:15:40 +0800

    fix(Index.vue/index.html): Todos改为 ToDo
     Todos改为 ToDo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="header-view w-auto">
       <h1 class="text-5xl text-center mt-10"> 👋 Hello </h1>
-      <h2 class="text-4xl text-blue-500 text-center mt-6">Todos</h2>
+      <h2 class="text-4xl text-blue-500 text-center mt-6">ToDo</h2>
     </div>
   </div>
 </template>
