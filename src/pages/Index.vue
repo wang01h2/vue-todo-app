6ef4d29f856bb09d9f126528237b419877797be3
wang01h2 <1299839398@qq.com>
2022-06-22 13:52:30 +0800

    fix(Index.vue): 手机item宽度不正常
    手机item宽度不正常

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,7 @@
       <h2 class="text-4xl text-blue-500 text-center my-6 xl:my-6 2xl:my-12">ToDo</h2>
     </div>
     <div class="flex justify-center items-center">
-      <index class="w-3/4 h-12 flex justify-center shadow bg-white xl:h-20 xl:w-4/12 2xl:h-20 w-4/12">
+      <index class="w-3/4 h-12 flex justify-center shadow bg-white xl:h-20 xl:w-4/12 2xl:h-20 2xl:w-4/12">
         <div class="flex justify-start items-center w-full">
           <div class="w-1/6 h-2/5 flex justify-center items-center">
             <!--          未选中-->
@@ -13,7 +13,6 @@
 <!--            选中-->
             <div class="w-5 h-5 ring-gray-200 bg-gray-500 border-gray-200 border-solid border-4 rounded-full xl:w-10 xl:h-10 xl:border-8"></div>
           </div>
-<!--          <input type="radio" class="w-1/6 h-2/5 border-none outline-none focus:outline-none focus:ring-2 focus:ring-purple-600">-->
           <span class="line-through truncate text-gray-400 text-left text-xl xl:text-4xl">早睡早起</span>
           <input type="text" class="line-through w-4/5 h-4/5 text-gray-600 border border-transparent focus:outline-none text-xl" placeholder="需要做些什么?" v-show="false">
         </div>
