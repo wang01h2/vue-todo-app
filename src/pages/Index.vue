5d8572ae61364d13a741b2638803b75dba03f8b6
wang01h2 <1299839398@qq.com>
2022-06-23 09:18:23 +0800

    fix(Index.vue/Item.vue): 整理到Item组件中
    整理到Item组件中

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,17 +6,8 @@
     </div>
     <div class="flex justify-center items-center">
       <index class="w-3/4 h-12 flex justify-center shadow bg-white xl:h-20 xl:w-4/12 2xl:h-20 2xl:w-4/12">
-        <div class="flex justify-start items-center w-full">
-          <div class="w-1/6 h-2/5 flex justify-center items-center">
-            <!--          未选中-->
-            <div class="w-5 h-5 border-gray-500 border-solid border-2 rounded-full" v-show="false"></div>
-<!--            选中-->
-            <div class="w-5 h-5 ring-gray-200 bg-gray-500 border-gray-200 border-solid border-4 rounded-full xl:w-10 xl:h-10 xl:border-8"></div>
-          </div>
-          <span class="line-through truncate text-gray-400 text-left text-xl xl:text-4xl">早睡早起</span>
-          <input type="text" class="line-through w-4/5 h-4/5 text-gray-600 border border-transparent focus:outline-none text-xl" placeholder="需要做些什么?" v-show="false">
-        </div>
-<!--        <item class="w-full"></item>-->
+<!--        item list-->
+        <item></item>
       </index>
     </div>
   </div>
@@ -25,5 +16,19 @@
 <script setup lang="ts">
 import Index from "../components/list/Index";
 import Item from "../components/list/Item";
+
+
+/*
+* 开始处理逻辑
+* 控制状态
+* :inputModel: 输入绑定数据，string类型
+*
+* 事件：
+* @isDone: 是否点击了完成 boolean
+* @inputComplete: input回车之后显示的状态 boolean
+*
+* */
+
+
 
 </script>
