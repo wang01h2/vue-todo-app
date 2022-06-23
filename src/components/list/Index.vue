c838f0e14d1f9f2f20dc64a387ee3a8f50d74d16
wang01h2 <1299839398@qq.com>
2022-06-23 13:06:14 +0800

    fix(Index.vue     Index.vue     Item.vue):  拆分为组件并完成功能
    拆分为组件并完成功能

--- src/components/list/Index.vue
+++ src/components/list/Index.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="index-view">
+    <div class="flex justify-start items-center w-full h-full shadow">
+      <div class="w-1/6 h-2/5 flex justify-center items-center"></div>
+      <input type="text" class="w-4/5 h-4/5 text-gray-600 border border-transparent focus:outline-none text-left text-xl xl:text-4xl" placeholder="需要做些什么?"
+             v-model.trim="inputValue"
+             @keyup.enter="inputCallBack">
+    </div>
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
+import {defineEmits, ref, watch} from "vue";
 
+interface Props {
+  inputModel: string,
+}
+const props = defineProps<Props>()
+let inputValue = ref('')
+const emits = defineEmits(['inputComplete'])
+watch(
+    () => props.inputModel,
+    (newValue) => {
+      inputValue.value = newValue
+    }
+)
+function inputCallBack() {
+  const obj = {
+    label: inputValue.value,
+    isDone: false,
+    key: Math.round(Math.random() * 900000).toString()
+  }
+  // 如果输入了内容才能返回callback
+  if(inputValue.value) emits('inputComplete', obj)
+  inputValue.value = ''
+}
 </script>
-
-<style scoped>
-
-</style>
--- src/components/list/Item.vue
+++ src/components/list/Item.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="flex justify-start items-center w-full">
+  <div class="flex justify-start items-center w-full h-full shadow bg-white">
     <div class="w-1/6 h-2/5 flex justify-center items-center">
       <!--          未选中-->
-      <div class="w-5 h-5 border-gray-500 border-solid border-2 rounded-full" v-show="false"></div>
+      <div class="w-5 h-5 border-gray-500 border-solid border-2 rounded-full cursor-pointer" v-show="!isDone" @click="unselected"></div>
       <!--            选中-->
-      <div class="w-5 h-5 ring-gray-200 bg-gray-500 border-gray-200 border-solid border-4 rounded-full xl:w-10 xl:h-10 xl:border-8"></div>
+      <div class="w-5 h-5 ring-gray-200 bg-gray-500 border-gray-200 border-solid border-4 rounded-full cursor-pointer xl:w-10 xl:h-10 xl:border-8" v-show="isDone"></div>
     </div>
-    <span class="line-through truncate text-gray-400 text-left text-xl xl:text-4xl">早睡早起</span>
-    <input type="text" class="line-through w-4/5 h-4/5 text-gray-600 border border-transparent focus:outline-none text-xl" placeholder="需要做些什么?" v-show="false">
+    <span :class="isDone?'line-through w-5/6 truncate text-gray-400 text-left text-xl xl:text-4xl':'w-5/6 truncate text-gray-600 text-left text-xl xl:text-4xl'">{{ label }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import {reactive, watch} from "vue";
 
+interface Props {
+  label?: string,
+  isDone?: boolean,
+  keyValue?: string
+}
+const props = defineProps<Props>()
+const itemData = reactive<Props>({
+      label: props.label,
+      isDone: props.isDone,
+      keyValue: props.keyValue
+    })
+const emits = defineEmits(['unselectedClick'])
+function unselected() {
+  itemData.isDone = !itemData.isDone
+  emits('unselectedClick', itemData)
+}
 </script>
-
-<style scoped>
-
-</style>
